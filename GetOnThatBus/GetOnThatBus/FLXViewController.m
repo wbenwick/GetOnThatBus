7a28a57584fa9f8faf8070acb6dfd72f11933e92
wbenwick <wbenwick@gmail.com>
2014-03-26 08:50:22 -0400

    Map View Displayed

--- GetOnThatBus/GetOnThatBus/FLXViewController.m
+++ GetOnThatBus/GetOnThatBus/FLXViewController.m
@@ -11,7 +11,8 @@
 
 @interface FLXViewController () <MKMapViewDelegate> {
     
-    __weak IBOutlet UIView *mapView;
+    __weak IBOutlet MKMapView *mapView;
+    __weak IBOutlet UIView *myView;
 }
 
 @end
@@ -21,7 +22,43 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    CLLocationCoordinate2D centerCoordinate = CLLocationCoordinate2DMake(32.13121, -81.17391);
+    
+    MKCoordinateSpan coordinateSpan = MKCoordinateSpanMake(1.0, 1.0);
+    
+    MKCoordinateRegion region = MKCoordinateRegionMake(centerCoordinate, coordinateSpan);
+    
+    mapView.region = region;
+    
+    MKPointAnnotation  *annotation = [MKPointAnnotation new];
+    
+    annotation.coordinate = centerCoordinate;
+    annotation.title = @"My spot";
+    
+    mapView.layer.shadowColor = [[UIColor blackColor] CGColor];
+    mapView.layer.shadowOffset = CGSizeMake(10.0f, 10.0f);
+    mapView.layer.shadowOpacity = 1.0f;
+    mapView.layer.shadowRadius = 10.0f;
+    
+    [mapView showsUserLocation];
+    
+    [mapView addAnnotation:annotation];
+    
+    CLGeocoder* geocoder = [CLGeocoder new];
+    
+    
+    [geocoder geocodeAddressString:@"Everbank Field" completionHandler:^(NSArray *placemarks, NSError *error)
+        {
+        
+            for (CLPlacemark* place in placemarks) {
+                MKPointAnnotation* annotation = [MKPointAnnotation new];
+                annotation.coordinate = place.location.coordinate;
+                [mapView addAnnotation:annotation];
+            }
+        }
+     
+     ];
 }
 
 - (void)didReceiveMemoryWarning
@@ -29,5 +66,41 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    NSLog(@"touched %@", [mapView userLocation]);
+    
+}
+
+-(MKAnnotationView *)mapView:(MKMapView *)myMapView viewForAnnotation:(id<MKAnnotation>)annotation
+{
+    
+    if (annotation == myMapView.userLocation) {
+        return nil;
+    }
+    
+    MKPinAnnotationView* pin = [[MKPinAnnotationView alloc]  initWithAnnotation:annotation reuseIdentifier:nil];
+    pin.image = [UIImage imageNamed:@"FileLogix"];
+    pin.frame = CGRectMake(0.0, 0.0, 25.0, 25.0);
+    
+    pin.animatesDrop = YES;
+    
+    [pin sizeToFit];
+    
+    pin.canShowCallout = YES;
+    
+    pin.rightCalloutAccessoryView = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
+    
+    return pin;
+}
+
+-(void)mapView:(MKMapView *)myMapView annotationView:(MKAnnotationView *)view calloutAccessoryControlTapped:(UIControl *)control
+{
+    NSLog(@"Tapped");
+    
+}
+
+
 
 @end
